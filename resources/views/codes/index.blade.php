2f6b35b1a1ec764cd96c83f8367666d670e6e11d
Daniel <daniel@omega323.com>
2017-04-24 17:23:06 +0200

    remove double meta viewport tag

--- resources/views/codes/index.blade.php
+++ resources/views/codes/index.blade.php
@@ -28,11 +28,7 @@
         type="text/css" 
         rel="stylesheet" 
         href="{{ asset('css/main.css') }}"/>
-
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
-
     <body>
       <div class='container'>
         <div class="row">
